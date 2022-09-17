75c963260aabbfcd4d7b71a3a283f31044f945c5
Iam maria theresa <iam.mariatheresaelkazzi@gmail.com>
2022-09-17 11:12:26 +0300

    done

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -14,4 +14,14 @@ function MovieCard({
   );
 }
 
+// function MovieCard({ title, posterSrc, genres }) {
+//   return (
+//     <div className="movie-card">
+//       <img src={posterSrc} alt={title} />
+//       <h2>{title}</h2>
+//       <small>{genres.join(", ")}</small>
+//     </div>
+//   );
+// }
+
 export default MovieCard;
